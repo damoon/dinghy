6d014d0d89f7071045eca3256c7e86b1ab4a649a
David Sauer <davedamoon@gmail.com>
2020-05-30 09:44:03 +0200

    fixup! wip to use tilt

--- annotate-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-nodes=$(kubectl get nodes -o go-template --template='{{range .items}}{{printf "%s\n" .metadata.name}}{{end}}')
-for node in $nodes; do
-  kubectl annotate node "${node}" \
-    --overwrite=true \
-    tilt.dev/registry=registry.guinan.31j.de \
-    tilt.dev/registry-from-cluster=registry.registry.svc:5000
-done
