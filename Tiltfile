6211e5304c05177484ee00fe4cbc4f54f79e616f
David Sauer <davedamoon@gmail.com>
2020-06-10 00:12:50 +0200

    sorted shared into services

--- Tiltfile
+++ Tiltfile
@@ -2,6 +2,6 @@
 disable_snapshots()
 allow_k8s_contexts(['test', 'ci'])
 
-include('./shared/Tiltfile')
+include('./e2e-tests/Tiltfile')
 include('./backend/Tiltfile')
 include('./frontend/Tiltfile')
--- backend/pkg/list_test.go
+++ backend/pkg/list_test.go
@@ -1,12 +1,15 @@
 package dinghy
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_shouldRedirect(t *testing.T) {
+func Test_parseRequest(t *testing.T) {
 	tests := []struct {
 		name     string
 		rawQuery string
 		want     bool
+		want1    bool
 		wantErr  bool
 	}{
 		{
@@ -32,16 +35,51 @@ func Test_shouldRedirect(t *testing.T) {
 			rawQuery: "redirect=0",
 			want:     true,
 		},
+		{
+			name:     "thumbnail set without value",
+			rawQuery: "thumbnail",
+			want1:    true,
+		},
+		{
+			name:     "thumbnail set to empty string",
+			rawQuery: "thumbnail=",
+			want1:    true,
+		},
+		{
+			name:     "thumbnail set to 1",
+			rawQuery: "thumbnail=1",
+			want1:    true,
+		},
+		{
+			name:     "thumbnail set to 0",
+			rawQuery: "thumbnail=0",
+			want1:    true,
+		},
+		{
+			name:     "redirect and thumbnail set",
+			rawQuery: "redirect&thumbnail",
+			want:     true,
+			want1:    true,
+		},
+		{
+			name:     "thumbnail and redirect set",
+			rawQuery: "thumbnail&redirect",
+			want:     true,
+			want1:    true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := shouldRedirect(tt.rawQuery)
+			got, got1, err := parseRequest(tt.rawQuery)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("shouldRedirect() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("parseRequest() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("shouldRedirect() = %v, want %v", got, tt.want)
+				t.Errorf("parseRequest() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("parseRequest() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
